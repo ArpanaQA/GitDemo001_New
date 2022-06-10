973ef2064684197f0502d487b657d14d99bfb642
Arpana <muriki.arpana@gmail.com>
2022-06-10 20:55:23 +0530

    First Commit

---
+++ src/main/java/Proj001/GitDemo001_new/App.java
@@ -0,0 +1,13 @@
+package Proj001.GitDemo001_new;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Proj001/GitDemo001_new/TestSce1.java
@@ -0,0 +1,10 @@
+package Proj001.GitDemo001_new;
+
+public class TestSce1 {
+
+	
+	public void launchApp()
+	{
+		System.out.println("Open Application");
+	}
+}
